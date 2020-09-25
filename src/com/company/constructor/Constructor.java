6b4b69d3a64b809319c8a2c66f8452b78ef0b77d
arasto <Arasto.Sahbaei@gmail.com>
2020-09-25 09:37:10 +0200

    m

--- src/com/company/constructor/Constructor.java
+++ src/com/company/constructor/Constructor.java
@@ -1,7 +1,14 @@
 package com.company.constructor;
 
 public class Constructor {
-    public static void main(String[] args) {
+    int x;
 
+    public Constructor() {
+        x = 5;
+    }
+
+    public static void main(String[] args) {
+    Constructor object = new Constructor();
+    System.out.println(object.x);
     }
 }
---
+++ src/ternaryoperator/Ternary.java
@@ -0,0 +1,12 @@
+package ternaryoperator;
+
+public class Ternary {
+
+    static int number = 1337;
+    static double number2 = 13.37;
+    static int result = number > number2 ? 1338 : 1339;
+
+    public static void main(String[] args) {
+        System.out.println(result);
+    }
+}
